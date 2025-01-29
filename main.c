4b1af5fafc73222cf5a5ebd23f994ea1772fce6c
Alisson Livio <livilia74629@gmail.com>
2025-01-29 14:04:01 -0300

    adição das bibliotecas

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "hardware/adc.h"
+#include "pico/bootrom.h"
 
-
+//Arquivo PIO - Configuração da Matriz de LED 
+#include "ws2812.pio.h";
 
 int main()
 {
